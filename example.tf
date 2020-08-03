a06d60649378cfb98a498eb8d3a11952db455bb5
Julie <67776121+juliehub@users.noreply.github.com>
2020-08-03 15:49:37 +1000

    Update example.tf

--- example.tf
+++ example.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0a58e22c727337c51" # Amazon Linux 2 AMI 2.0.20200617.0 x86_64 HVM gp2
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
