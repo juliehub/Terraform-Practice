e4c0c43daf51e0f6f39e6d5b7b6a2e03945b73ec
Julie <67776121+juliehub@users.noreply.github.com>
2020-08-03 10:18:16 +1000

    Create example.tf

---
+++ example.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a58e22c727337c51" # Amazon Linux 2 AMI 2.0.20200617.0 x86_64 HVM gp2
+  instance_type = "t2.micro"
+}
