00ca5fac53f9d3fc86282630fb085751000a65f9
Julie <67776121+juliehub@users.noreply.github.com>
2020-08-08 20:57:41 +1000

    Create example_input_var.tf

---
+++ example_input_var.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile    = "default"
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.amis[var.region]
+  instance_type = "t2.micro"
+}
+
+output "ami" {
+  value = aws_instance.example.ami
+}
