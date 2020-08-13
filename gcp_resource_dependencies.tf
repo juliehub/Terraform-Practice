207911f28c63920bacba11395a19b0adcbca1512
Julie <67776121+juliehub@users.noreply.github.com>
2020-08-13 17:04:41 +1000

    Create gcp_resource_dependencies.tf

---
+++ gcp_resource_dependencies.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("MyFirstProject-123456789012.json")
+
+  project = "indigo-winter-286100"
+  region  = "australia-southeast1"
+  zone    = "australia-southeast1-b	
+"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
